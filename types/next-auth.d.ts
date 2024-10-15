16614f017254775e5bf92ea4adfea0a189b0dbff
Rayane Marques <mmarquesraioane586@gmail.com>
2024-10-15 03:40:46 -0300

    feat: include roile in Session types

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,6 +4,7 @@ declare module 'next-auth' {
   interface Session extends DefaultSession {
     user: DefaultSession['user'] & {
       id: string
+      role?: string | null
       token?: accessToken
     }
   }
